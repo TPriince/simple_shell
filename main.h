6986655ac4c93259b35fb7d0b7edcaa9974ef6e5
TPriince <tobiloba110@gmail.com>
2022-05-10 00:32:17 -0700

    chore: add string functions and header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* str_funcs1 */
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *strdup(char *str);
+char *_strcpy(char *dest, char *src);
+
+#endif
