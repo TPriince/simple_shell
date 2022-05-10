08b928193d041441dae76c4109b0f341a040e5c4
TPriince <tobiloba110@gmail.com>
2022-05-10 01:14:13 -0700

    chore: add more string functions

--- main.h
+++ main.h
@@ -8,4 +8,10 @@ int _strcmp(char *s1, char *s2);
 char *strdup(char *str);
 char *_strcpy(char *dest, char *src);
 
+/* str_funcs2 */
+char *_strtok(char *str, char *delim);
+int _strcspn(char *string, char *chars);
+char *_strchr(char *s, char c);
+
+
 #endif
